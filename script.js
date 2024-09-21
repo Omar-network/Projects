3b2624e254ab98f3db409b5140860234191079d2
Omar-network <omar.lakbaich09@gmail.com>
2024-09-22 01:01:53 +0200

    first

---
+++ script.js
@@ -0,0 +1,15 @@
+let ratting = 3
+if (ratting === 3){
+    console.log("YOU ARE A SUPERSTAR!");
+}
+
+// Logical AND :
+const password = prompt("Entrer your password");
+if (password.length >= 6 && password.indexOf('') === -1){
+     console.log("VALID PASSWORD"!)
+     { else }
+     console.log("INCORRECT FORMAT FOR PASSWORD!")
+
+}
+
+// Logical OR : if one side is true, the entire thing is true
